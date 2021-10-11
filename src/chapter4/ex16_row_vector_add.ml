c19d9d28c8dc1d04aa02f4b60c27e4e2f68e038f
Denis Strelkov <strelkov.d.d@gmail.com>
2021-10-11 11:03:49 +0300

    Chapter 4 ex 16: add row vectors

---
+++ src/chapter4/ex16_row_vector_add.ml
@@ -0,0 +1,38 @@
+(* Exercise: row vector add [★★★]
+
+   Implement a function add_row_vectors: int list -> int list -> int list
+   for the element-wise addition of two row vectors.
+   For example, the addition of [1; 1; 1] and [9; 8; 7] is [10; 9; 8].
+   If the two vectors do not have the same number of entries, the behavior of your function is unspecified—that is,
+   it may do whatever you like.
+   Hint: there is an elegant one-line solution using List.map2. Unit test the function. *)
+
+let add_row_vectors v1 v2 =
+  try List.map2 (fun a b -> a + b) v1 v2
+  with _ -> failwith "add_row_vectors: different size of vectors"
+
+module Test = struct
+  open OUnit2
+
+  let make_test test_name expected_output (row1, row2) =
+    test_name >:: fun _ ->
+    assert_equal expected_output (add_row_vectors row1 row2)
+
+  let make_test_exn test_name expected_exn (row1, row2) =
+    test_name >:: fun _ ->
+    assert_raises expected_exn (fun () -> add_row_vectors row1 row2)
+
+  let tests =
+    "tests for add_row_vectors function"
+    >::: [
+           make_test "valid, both rows are empty" [] ([], []);
+           make_test "valid, rows with single element" [ 3 ] ([ 1 ], [ 2 ]);
+           make_test "valid, rows with three elements" [ 10; 9; 8 ]
+             ([ 1; 1; 1 ], [ 9; 8; 7 ]);
+           make_test_exn "invalid, different row sizes"
+             (Failure "add_row_vectors: different size of vectors")
+             ([], [ 1 ]);
+         ]
+
+  let test () = run_test_tt_main tests
+end
--- src/chapter4/test.ml
+++ src/chapter4/test.ml
@@ -1,3 +1,6 @@
 let test () =
-  Ex10_account_balance.test ();
-  Ex15_valid_matrix.Test.test ()
+  (* Ex10_account_balance.test (); *)
+  (* Ex15_valid_matrix.Test.test () *)
+  Ex16_row_vector_add.Test.test ()
+(* Ex17_matrix_add.Test.test (); *)
+(* Ex18_matrix_multiply.Test.test (); *)
